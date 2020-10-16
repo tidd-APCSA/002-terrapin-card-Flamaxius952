85901023a825b289e68d74c9a21245b85f84e514
Flamaxius952 <overlyedgydragon952@gmail.com>
2020-10-16 18:24:45 +0000

    finished assignment

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,7 +1,31 @@
 public class CardRunner {
-    public static void main(String args[]){
-        // ☕
-        System.out.println("hey");
+  public static void main(String args[]){
+    TerrapinCard cardBilly = new TerrapinCard (20);
+    TerrapinCard cardBrian = new TerrapinCard (30);
 
-    }
+    cardBilly.payGourmet();
+    cardBrian.payEconomical();
+
+    System.out.print("Billy: ");
+    cardBilly.toString();
+    System.out.print("Brian: ");
+    cardBrian.toString();
+
+    cardBilly.loadMoney(20);
+    cardBrian.payGourmet();
+
+    System.out.print("Billy: ");
+    cardBilly.toString();
+    System.out.print("Brian: ");
+    cardBrian.toString();
+
+    cardBilly.payEconomical();
+    cardBilly.payEconomical();
+    cardBrian.loadMoney(50);
+
+    System.out.print("Billy: ");
+    cardBilly.toString();
+    System.out.print("Brian: ");
+    cardBrian.toString();
+  }
 }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,39 @@
-public class TerrapinCard {
+public class TerrapinCard{
 
+  private double balance;
 
+  public TerrapinCard(double cBalance) {
+    // write code here
+    balance = cBalance;
+  }
+
+  public String toString() {
+    // write code here
+    System.out.println("The card has " + balance + " dollars");
+    return "The card has " + balance + " dollars";
+  }
+
+  public void payEconomical() {
+    // write code here
+    if ((balance - 2.50) >= 0.00)
+    {
+    balance = balance - 2.50;
+    }
+  }
+
+  public void payGourmet() {
+    // write code here
+    if ((balance - 4.00) >= 0.00)
+    {
+    balance = balance - 4.00;
+    }
+  }
+
+  public void loadMoney(double amount) {
+    //write code here
+    if (amount > 0)
+    {
+      balance = balance + amount;
+    }
+  }
 }
